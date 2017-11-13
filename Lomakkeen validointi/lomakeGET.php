ab425fa2a3c87fb79402100b9dbfa8724c8cb50b
Slicksy <33019310+Slicksy@users.noreply.github.com>
2017-11-13 09:55:50 +0200

    Add files via upload

---
+++ Lomakkeen validointi/lomakeGET.php
@@ -0,0 +1,11 @@
+<p><?php echo $_GET["firstname"]; ?></p>
+
+<?php
+echo "<p>".$_GET["lastname"]."</p>";
+echo $_GET["email"];
+echo $_GET["Number"];
+echo $_GET["Address"];
+echo $_GET["Zip"];
+echo $_GET["Password"];
+
+?>
